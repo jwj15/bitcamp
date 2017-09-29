5f017d0f4fed8f61f15f216dc6a09c90a8bfe3a4
jwj15 <jwj15@naver.com>
2017-09-29 17:21:03 +0900

    '-'

---
+++ java-basic/src/main/java/bitcamp/java100/Test.java
@@ -0,0 +1,9 @@
+// 테스트용
+package bitcamp.java100;
+
+public class Test {
+
+        public static void main(String[] args) {
+               // float연산
+              }
+            }
--- java-project/src/main/java/App.java
+++ java-project/src/main/java/App.java
@@ -14,22 +14,40 @@
 // 평균: 100.0
 // ver 01
 // 표준 출력장치로 인사문구 출력하기
+//
+// ver04
+// 여러 명의 성적을 저장하고 다음과 간이 출력하라!
+// -- 출력내용
+// 홍길동, 100,  90,  80, 270, 90.0
+// 임꺽정,  80,  80,  80, 240, 80.0
+// 유관순, 100, 100, 100, 300, 100.0
 public class App {
     
     public static void main(String[] args) {
         // 변수를 선언하여 값은 보관한다.
-        String name = "홍길동";
-        int kor = 100;
-        int eng = 97;
-        int math =98;
-        int sum = kor + eng + math;
-        float avar = sum / 3.0f;
-        System.out.printf("이름: %s\n", name); 
-        System.out.printf("국어: %d\n", kor);
-        System.out.printf("영어: %d\n", eng);
-        System.out.printf("수학: %d\n", math);
-        System.out.printf("총점: %d\n", sum);
-        System.out.printf("평균: %.1f\n", avar);
+        String[] names={"홍길동","임꺽정","유관순"};
+        int[] kor={100,80,100};
+        int[] eng={90,80,100};
+        int[] math={80,80,100};
+
+        for (int i=0; i<names.length; i++){
+            int sum=kor[i]+eng[i]+math[i];
+            float avar=sum/3;
+            System.out.printf("%-4s,%4d,%4d,%4d,%4d,%5.1f\n",names[i],kor[i],eng[i],math[i],sum,avar);
+        }
+              
+       // String name = "홍길동";
+       // int kor = 100;
+       // int eng = 97;
+       // int math =98;
+       // int sum = kor + eng + math;
+       // float avar = sum / 3.0f;
+       // System.out.printf("이름: %s\n", name); 
+       // System.out.printf("국어: %d\n", kor);
+       // System.out.printf("영어: %d\n", eng);
+       // System.out.printf("수학: %d\n", math);
+       // System.out.printf("총점: %d\n", sum);
+       // System.out.printf("평균: %.1f\n", avar);
         
     }
 }
