3a500936a851d19ea5b6ae2b56c477320d36cc52
jwj15 <jwj15@naver.com>
2017-10-11 17:33:39 +0900

    ..

--- java-basic/src/main/java/bitcamp/java100/Test15_11_1.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_11_1.java
@@ -1,5 +1,5 @@
-// ## 연산자 - 비트 연산자 (&, |, ~, ^)
-// - 비트 연산자의 사용법을 알아보자!
+// ## 연산자 - 비트 연산자 (&, |, ~, ^) 응용
+// - 비트 연산자의 활용 예
 //
 
 package bitcamp.java100;
@@ -7,8 +7,11 @@ package bitcamp.java100;
 public class Test15_11_1 {
 
     public static void main(String[] args) {
-        //임무: 파일의 정보를 저장하라!
-        // - 디렉토리여부, 소유주/그룹/기타 사용자의 사용권한(읽기, 쓰기, 실행 권한 정보)
+// 임무: 파일의 정보를 저장하라!
+// - 디렉토리여부, 소유주/그룹/기타 사용자의 읽기, 쓰기, 실행 권한 정보
+//
+// 방법1: 클래스를 이용하여 처리        
+        // => 메모리 설계
         class Privilege {
             boolean isDirectory;
             boolean ownerRead;
@@ -19,9 +22,13 @@ public class Test15_11_1 {
             boolean groupExe;
             boolean otherRead;
             boolean otherWrite;
-            boolean otherExe;   
+            boolean otherExe;
         }
+        
+        // => Privilege 설계도에 따라 메모리를 준비하고 그 메모리의 주소를 저장한다.
         Privilege p1 = new Privilege();
+        
+        // => 메모리 주소(레퍼런스)를 가지고 해당 메모리의 각 항목에 값을 넣는다.
         p1.isDirectory = false;
         p1.ownerRead = true;
         p1.ownerWrite = true;
@@ -32,7 +39,8 @@ public class Test15_11_1 {
         p1.otherRead = true;
         p1.otherWrite = false;
         p1.otherExe = true;
-
+        
+        // => 사용 권한 검사
         System.out.print(p1.isDirectory ? "d" : "-");
         System.out.print(p1.ownerRead ? "r" : "-");
         System.out.print(p1.ownerWrite ? "w" : "-");
@@ -44,6 +52,26 @@ public class Test15_11_1 {
         System.out.print(p1.otherWrite ? "w" : "-");
         System.out.print(p1.otherExe ? "x" : "-");
         System.out.println();
-
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
