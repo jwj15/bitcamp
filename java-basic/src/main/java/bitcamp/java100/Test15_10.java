1e5b633abded58dc3cfde2a2308968422c220238
jwj15 <jwj15@naver.com>
2017-10-11 11:46:16 +0900

    .

--- java-basic/src/main/java/bitcamp/java100/Test15_10.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_10.java
@@ -9,18 +9,18 @@ public class Test15_10 {
     public static void main(String[] args) { 
         int v1 = 0b0101_1100;
         int v2 = 0b0110_0110;
-        
-// &(논리곱 AND)
-// => 각 비트끼리 AND 연산을 수행한다. 둘 다 1일 때만 결과는 1이 된다.
+
+        // &(논리곱 AND)
+        // => 각 비트끼리 AND 연산을 수행한다. 둘 다 1일 때만 결과는 1이 된다.
         int result = v1 & v2;
         System.out.println(java.lang.Integer.toBinaryString(result));
-        
+
         result = v1 | v2;
         System.out.println(java.lang.Integer.toBinaryString(result));
-        
+
         result = v1 ^ v2;
         System.out.println(java.lang.Integer.toBinaryString(result));
-        
+
         result = ~v1;
         System.out.println(java.lang.Integer.toBinaryString(result));
     }
